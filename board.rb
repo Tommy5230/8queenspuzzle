1960a99742d25ecb43f9ac9d91b5f0a9e410163d
Tommy5230 <tomasz.donarski@naturaily.com>
2021-01-05 19:14:54 +0100

    Add first and last column methods

--- board.rb
+++ board.rb
@@ -21,7 +21,7 @@ class Board
     board.first(size)
   end
 
-  def first_row_skip_corners_indexes
+  def first_row_skip_corners_ids
     first_row_skip_corners = first_row.map.with_index { |_pos, i| i }
     first_row_skip_corners[1..-2]
   end
@@ -30,9 +30,27 @@ class Board
     board.last(size)
   end
 
-  def last_row_skip_corners_indexes
+  def last_row_skip_corners_ids
     last_row_skip_corners = last_row.map.with_index { |_pos, i| i }
     last_row_skip_corners[1..-2]
+  end
+
+  def first_column
+    board.select.with_index { |_, i| ((i + size) % size).zero? }
+  end
+
+  def first_column_skip_corners_ids
+    first_column_skip_corners = first_column.map.with_index { |_pos, i| i }
+    first_column_skip_corners[1..-2]
+  end
+
+  def last_column
+    board.select.with_index { |_, i| ((i + size) % size) == size - 1 }
+  end
+
+  def last_column_skip_corners_ids
+    last_column_skip_corners = last_column.map.with_index { |_pos, i| i }
+    last_column_skip_corners[1..-2]
   end
 
   # def boundaries(size)
--- test.rb
+++ test.rb
@@ -5,4 +5,7 @@ require 'pry'
 require_relative 'board'
 
 b = Board.new(8)
-print b.last_row_skip_corners_indexes
+# print b.board
+# print b.last_row_skip_corners_indexes
+# print b.first_column
+print b.last_column
