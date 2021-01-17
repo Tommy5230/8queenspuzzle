80b0fce9256d69fc64968ff180628263ac213621
Tommy5230 <tomasz.donarski@naturaily.com>
2021-01-17 13:51:15 +0100

    Add basic loop structure

--- solver.rb
+++ solver.rb
@@ -21,33 +21,41 @@ class Solver
   def place_the_queen(position)
     queen = Queen.new(board_size, position)
     main_board.fields -= queen.covered_fields
-    # puts $main_board.fields.count
-    queen.plot_fields
   end
 
   def fields_count
     main_board.fields.count
   end
 
-  def queen_placing_loop
-    while fields_count > 0 do
+  def queen_placing_loop(starting_field)
+    solution_accu = []
+    puts "~~ Starting field - #{starting_field} ~~"
+    puts ' '
+    place_the_queen(starting_field)
+    solution_accu << starting_field
+    while fields_count.positive? # fields.count > 0
       next_free_field = main_board.fields.first
+      puts "Next free field - #{next_free_field}"
+      puts ' '
       place_the_queen(next_free_field)
       solution_accu << next_free_field
-      puts "iteration - #{next_free_field}"
-      puts ' '
     end
+    if solution_accu.count == 8
+      print "#{solution_accu} ~~~~~ SOLUTION FOUND! ~~~~~"
+    else
+      print solution_accu # after the last of this - it ran out of fields
+    end
+    puts ' '
   end
 
-  # def method_to_be_named
-  #   position =
-  #   place_the_queen(position)
-  # end
-
-  # main_board.fields.map do |field|
-  #   # each of the fields will serve as a field for placing the first queen
-  #   queen_placing_loop
-  #   print solution_accu
-  #   main_board_reset
-  # end
+  def run
+    main_board.fields.map do |field|
+      # each of the fields will serve as a field for placing the first queen
+      queen_placing_loop(field)
+      main_board_reset
+      puts "Finished loop for ===#{field}==="
+      puts ' '
+    end
+    puts 'End of the loop'
+  end
 end
